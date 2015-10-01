4380a9998c4ab8ad14224ff395a3334ccc99d566
IFours <ilia.sachok@gmail.com>
2015-10-01 11:12:24 +0300

    Move to react 0.11.

---
+++ components/ChannelGridItem.js
@@ -0,0 +1,25 @@
+/* @flow */
+'use strict';
+
+var React = require('react-native');
+
+var {
+  StyleSheet,
+  View,
+} = React;
+
+var Component = React.createClass({
+  render: function() {
+    return (
+      <View />
+    );
+  }
+});
+
+
+var styles = StyleSheet.create({
+
+});
+
+
+module.exports = Component;
